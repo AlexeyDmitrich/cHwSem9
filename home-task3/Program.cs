b91b18321ca8219b0dc98e3a5efbda5ea657cb05
Alexey <a.d.kamenev@yandex.ru>
2022-12-22 16:10:13 +0300

    3rd task completed

--- home-task3/Program.cs
+++ home-task3/Program.cs
@@ -10,3 +10,12 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
 
+int  Akkerman (int n, int m)
+   if (n == 0){
+    return m + 1
+}
+   else if (m == 0) {
+    return Akkerman (n - 1, 1)
+}
+   else
+    return Akkerman(n - 1, ack (n, m - 1))
